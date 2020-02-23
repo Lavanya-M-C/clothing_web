4b317dda33d3e100d759e579ecc95f3a4a903dfc
root <mc.lavanya@mahindra.com>
2020-02-23 12:26:28 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
