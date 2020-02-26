871082188b71d8400ef5c2170d981326a68801f0
root <mc.lavanya@mahindra.com>
2020-02-26 09:26:53 +0530

    form-input and custom-button component is created

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './form-input.styles.scss';
+
+const FormInput = ({ label, handelChange, ...otherProps }) => (
+    <div className='group'>
+        <input className='form-input' onchange={handelChange} {...otherProps} />
+
+        {
+            label ?
+            (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                {label}
+            </label>) 
+            : null
+        }
+
+    </div>
+);
+
+export default FormInput;
