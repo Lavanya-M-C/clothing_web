8d7034886816fe086e933239d109acd372d24576
root <mc.lavanya@mahindra.com>
2020-05-06 12:41:31 +0530

    Updates

---
+++ precache-manifest.3cb584ebad5d21775784b2146ae445c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac3729341c63063c8c855f19b92143f",
+    "url": "/clothing_web/index.html"
+  },
+  {
+    "revision": "4d63c0be424d507a1099",
+    "url": "/clothing_web/static/css/main.6048a5a1.chunk.css"
+  },
+  {
+    "revision": "1f53792b9f1157bec726",
+    "url": "/clothing_web/static/js/2.bc04f3e4.chunk.js"
+  },
+  {
+    "revision": "4691121c96f91c7f8618b1ecc9480cfc",
+    "url": "/clothing_web/static/js/2.bc04f3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d63c0be424d507a1099",
+    "url": "/clothing_web/static/js/main.009c6f87.chunk.js"
+  },
+  {
+    "revision": "33a54a588ff2044861bc",
+    "url": "/clothing_web/static/js/runtime-main.e77f9c0a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing_web/static/media/crown.ed28accb.svg"
+  }
+]);
